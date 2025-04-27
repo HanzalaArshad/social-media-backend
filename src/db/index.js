17a9731c8bbe81a312b4c71bb3ae34706653832d
HanzalaArshad <hanzalaarshad338@gmail.com>
2025-04-27 19:37:56 +0500

    db connected

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+
+
+const connectDB=async ()=>{
+  try {
+   const ConnectionInstance= await mongoose.connect(`${process.env.MONGO_DB_URL}/${DB_NAME}`)
+   console.log(`\nMongoDB Connected !! DB_HOST ${ConnectionInstance.connection.host}`);
+   
+  } catch (error) {
+    console.log("MongoDB Connection Error",error);
+    process.exit(1)
+    
+  }
+}
+
+
+
+export default connectDB;
