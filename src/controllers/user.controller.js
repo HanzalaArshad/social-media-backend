f25ad636ebe21e4b7dc017657016a403c0c17e20
HanzalaArshad <hanzalaarshad338@gmail.com>
2025-06-17 21:14:53 +0500

    `Added refresh token functionality and updated related code`

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import ApiError from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
 
 
 
@@ -12,8 +13,8 @@ const genrateAccessAndRefreshTokens=async (userId)=>{
     try {
       const user=await User.findById(userId);
 
-      const accessToken=user.genrateAccessToken()
-      const refreshToken=user.genrateRefreshToken()
+      const accessToken=user.generateAccessToken()
+      const refreshToken=user.generateRefreshToken()
 
       // adding the refreshToken
       user.refreshToken=refreshToken;
@@ -125,7 +126,7 @@ const loginUser=asyncHandler(async(req,res)=>{
 
    const {email,password,username}=req.body
 
-   if(!email || !username){
+   if(!email && !username){
     throw new ApiError(400,"Username or Email is Required")
    }
 
@@ -153,7 +154,7 @@ const loginUser=asyncHandler(async(req,res)=>{
 
    // ab actually mn user obj oper hai tou wahan sab field ka access hai isliye hum dobara query chal rhe hain
 
-   const loggedInUser=await User.findById(user._id).select("-password, -refreshToken")
+   const loggedInUser=await User.findById(user._id).select("-password -refreshToken")
  
 
    // sending cookies 
@@ -202,4 +203,42 @@ const logOutUser=asyncHandler(async(req,res)=>{
   
 })
 
-export { registerUser,loginUser,logOutUser };
+
+
+const refreshAccessToken=asyncHandler(async(req,res)=>{
+  const incomingRefreshToken=req.cookies.refreshToken || req.body.refreshToken
+
+  if(!incomingRefreshToken){
+    throw new ApiError(401,"unAuthorized Request")
+
+  }
+
+  try {
+    const decodedToken=jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET)
+  
+    const user=await User.findById(decodedToken?._id)
+  
+    if(!user){
+      throw new ApiError(401,"invalid refresh token")
+  
+    }
+  
+    if(incomingRefreshToken != user.refreshToken){
+      throw new ApiError(401,"refresh token is expired")
+    }
+  
+    const options={
+      httpOnly:true,
+      secure:true
+    }
+  
+  
+    const{accessToken,newRefreshToken}=await genrateAccessAndRefreshTokens(user._id)
+    return res.status(200).cookie("accessToken",accessToken,options).cookie("refreshToken",newRefreshToken,options)
+    .json(new ApiResponse(200,{accessToken,refreshToken:newRefreshToken},"access token Refreshed"))
+  } catch (error) {
+    throw ApiError (401,error?.message || "Invalid Refresh Token")
+  }
+
+})
+export { registerUser,loginUser,logOutUser,refreshAccessToken };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -69,37 +69,32 @@ userSchema.methods.isPasswordCorrect=async function(password){
 }
 
 
-userSchema.methods.genrateAccessToken=function(){
-    jwt.sign(
-      {
-        _id:this._id,
-        email:this.email,
-        username:this.username,
-        fullName:this.fullName,
-         
-
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
     },
     process.env.ACCESS_TOKEN_SECRET,
     {
-      expiresIn:process.env.ACEESS_TOKEN_EXPIRY
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY, // also fix typo in variable name
     }
-  )
-}
+  );
+};
 
-userSchema.methods.genrateRefreshToken=function(){
 
-  jwt.sign(
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
     {
-      _id:this._id,
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
-       
-
-  },
-  process.env.REFRESH_TOKEN_SECRET, 
-  {
-    expiresIn:process.env.REFRESH_TOKEN_EXPIRY
-  }
-)
-
-}
 export const User=mongoose.model("User",userSchema)
